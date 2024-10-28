f57afb1db3dd0e0520adfc5c07cbaf03aecbedbb
Michael Maluf <malufmike@gmail.com>
2024-10-28 02:53:42 -0700

    test 1

--- sdn-firewall.py
+++ sdn-firewall.py
@@ -12,6 +12,15 @@ from pox.lib.addresses import IPAddr, EthAddr
 # may need to complete your firewall implementation.  No additional functions "should" be required to complete
 # this assignment.
 
+# class ofp_flow_mod(ofp_header):
+#     def __init__(self, **kw):
+#         ofp_header.__init__(self)
+#         self.header_type = OFPT_FLOW_MOD
+#         self.match = ofp_match()
+#         self.priority = OFP_DEFAULT_PRIORITY
+#         self.actions = []
+
+
 
 def firewall_policy_processing(policies):
     '''
@@ -38,18 +47,40 @@ def firewall_policy_processing(policies):
     '''
 
     rules = []
+    DL_TYPE = 0x800
 
     for policy in policies:
         # Enter your code here to implement matching and block/allow rules.  See the links
         # in Implementation Hints on how to do this. 
         # HINT:  Think about how to use the priority in your flow modification.
 
-        rule = None # Please note that you need to redefine this variable below to create a valid POX Flow Modification Object
+        rule = of.ofp_flow_mod()
+        rule.match.dl_type = DL_TYPE
+
+        if policy['mac-src'] != '-':
+            rule.match.dl_src = EthAddr(policy['mac-src'])
+        if policy['mac-dst'] != '-':
+            rule.match.dl_dst = EthAddr(policy['mac-dst'])
+        if policy['ip-src'] != '-':
+            rule.match.nw_src = policy['ip-src']
+        if policy['ip-dst'] != '-':
+            rule.match.dl_src = policy['ip-dst']
+        if policy['ipprotocol'] != '-':
+            rule.match.nw_proto = int(policy['ipprotocol'])
+        if policy['port-src'] != '-':
+            rule.match.tp_src = int(policy['port-src'])
+        if policy['port-dst'] != '-':
+            rule.match.tp_dst = int(policy['port-dst'])
+
+        if policy['action'] == 'Allow':
+            rule.priority = 10000
+        else:
+            rule.priority = 0
 
 
         # End Code Here
         print('Added Rule ',policy['rulenum'],': ',policy['comment'])
-        #print(rule)   #Uncomment this to debug your "rule"
+        print(rule)   #Uncomment this to debug your "rule"
         rules.append(rule)
     
     return rules
